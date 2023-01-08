0009456c3819f53150f2d6fde858a0e6bd5f03c0
Sosedova Viktoria <nvikamail@mail.ru>
2023-01-08 19:58:29 +0400

    fix: updated pictures

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "../styles.css"
-import "./assets/images/308581.svg"
-import "./assets/images/like=like.svg"
-import "./assets/images/compare_active.svg"
-import "./assets/images/cart=empty.svg"
+import "./assets/common/308581.svg"
+import "./assets/common/like=like.svg"
+import "./assets/common/compare_active.svg"
+import "./assets/common/cart=empty.svg"
 
-import "./assets/images/GitHub-Mark-Light-64px.png"
-import "./assets/images/rs_school_js.svg"
+import "./assets/social/GitHub-Mark-Light-64px.png"
+import "./assets/social/rs_school_js.svg"
 
 console.log("Hello World!");

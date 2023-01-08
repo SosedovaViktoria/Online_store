0eed6ca467476c2e85d0ccd1ef0ec9047689d97c
Sosedova Viktoria <nvikamail@mail.ru>
2023-01-08 17:35:32 +0400

    fix: add pictures and fix webpack for pictures

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import "../styles.css"
+import "./assets/images/308581.svg"
+import "./assets/images/like=like.svg"
+import "./assets/images/compare_active.svg"
+import "./assets/images/cart=empty.svg"
+
+import "./assets/images/GitHub-Mark-Light-64px.png"
+import "./assets/images/rs_school_js.svg"
 
 console.log("Hello World!");

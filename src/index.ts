3c168f1c378867616711cb9674b021e91b29d755
Sosedova Viktoria <nvikamail@mail.ru>
2023-01-09 22:11:38 +0400

    feat: update resolved merge conflicts

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import './assets/styles/productItem.css'
 
 import "../styles.css"
 import "./assets/common/308581.svg"
-import "./assets/common/like=like.svg"
+import "./assets/common/like.svg"
 import "./assets/common/compare_active.svg"
-import "./assets/common/cart=empty.svg"
-import "./assets/social/GitHub-Mark-Light-64px.png"
+import "./assets/common/cart_empty.svg"
+import "./assets/social/github-mark.svg"
 import "./assets/social/rs_school_js.svg"
 
 
